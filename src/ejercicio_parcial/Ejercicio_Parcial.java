e1ef00bc722ac4e3b2799805831a394a93c064bd
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-05-22 16:51:47 -0500

    ejercio del parcial

---
+++ src/ejercicio_parcial/Ejercicio_Parcial.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_parcial;
+
+import Archivos.Archivo;
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercicio_Parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+       Archivo archivo=new Archivo();
+       archivo.promedio();
+    }
+    
+}
